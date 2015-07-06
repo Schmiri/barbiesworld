3d8e85812677f985fb5512a08da4dee47e7382f8
mimamichelle <rotter.michelle@gmail.com>
2015-07-06 14:05:45 +0200

    flyhigh150706_v4

--- Boot.js
+++ Boot.js
@@ -65,8 +65,8 @@ BasicGame.Boot.prototype = {
     preload: function () {
 
         //  Here we load the assets required for our preloader (in this case a background and a loading bar)
-        this.load.image('preloaderBackground', 'assets/preloader_background.png');
-        this.load.image('preloaderBar', 'assets/preloadr_bar.png');
+        //this.load.image('preloaderBackground', 'assets/preloader_background.png');
+        //.load.image('preloaderBar', 'assets/preloadr_bar.png');
 
     },
 
