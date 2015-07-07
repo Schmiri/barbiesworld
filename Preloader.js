bc86ef5b09aaa36c55918d65e86651266d0987ca
mimamichelle <rotter.michelle@gmail.com>
2015-07-07 14:50:59 +0200

    flyhigh150707_v5

--- Preloader.js
+++ Preloader.js
@@ -1,9 +1,5 @@
 BasicGame.Preloader = function (game) {
-
-    //this.ready = false;
-
-
-
+    this.ready = false;
 };
 
 BasicGame.Preloader.prototype = {
@@ -11,13 +7,8 @@ BasicGame.Preloader.prototype = {
     preload: function () {
 
         //  These are the assets we loaded in Boot.js
-        //  A nice sparkly background and a loading progress bar
         this.stage.backgroundColor = '#9EDEF2';
      
-        //  This sets the preloadBar sprite as a loader sprite.
-        //  What that does is automatically crop the sprite from 0 to full-width
-        //  as the files below are loaded in.
-
         //  Here we load the rest of the assets our game needs.
         //  As this is just a Project Template I've not provided these assets, swap them for your own.
         this.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
@@ -31,38 +22,19 @@ BasicGame.Preloader.prototype = {
         this.load.image('reloadButton', 'assets/reload_button.png');
         this.load.image('nextButton', 'assets/next_button.png');
         this.load.image('tutorialButton', 'assets/tutorial_button.png');
-
-
-        //this.load.audio('titleMusic', ['audio/main_menu.mp3']);
-
+        this.load.audio('titleMusic', ['assets/main_menu.mp3']);
     },
 
 
     create: function () {
-
-        //  Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
-        //this.preloadBar.cropEnabled = false;
-          
-
     },
 
     update: function () {
-
-        //  You don't actually need to do this, but I find it gives a much smoother game experience.
-        //  Basically it will wait for our audio file to be decoded before proceeding to the MainMenu.
-        //  You can jump right into the menu if you want and still play the music, but you'll have a few
-        //  seconds of delay while the mp3 decodes - so if you need your music to be in-sync with your menu
-        //  it's best to wait for it to decode here first, then carry on.
-        
-        //  If you don't have any music in your game then put the game.state.start line into the create function and delete
-        //  the update function completely.
-        
-        /*if (this.cache.isSoundDecoded('titleMusic') && this.ready == false)
+        if (this.cache.isSoundDecoded('titleMusic') && this.ready == false)
         {
-            this.ready = true;*/
+            this.ready = true;
             this.state.start('MainMenu');
-        //}
+        }
 
     }
-
 };
